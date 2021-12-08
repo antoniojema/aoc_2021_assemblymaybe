f71a5f16d2face60cbb1c5792b7b8b114b50ad39
Antonio <antoniojema@gmail.com>
2021-12-08 13:19:18 +0100

    day 03 solved

--- aoc/day03/part2.asm
+++ aoc/day03/part2.asm
@@ -1,14 +1,14 @@
 global aoc_day03_2
 
-extern printcstr, printlb, printd, file_read_line
+extern printcstr, printlb, printd, file_read_line, cstr_read_bits
 
 section .data
-        filename  db      'sample2.txt', 0
-        part1     db      'Part 2:', 0
+        filename  db      'input2.txt', 0
+        part      db      'Part 2:', 0
         file_des  db      '    File descriptor: ', 0
         lines     db      '    Lines read:      ', 0
-        epsil     db      '    epsilon:         ', 0
-        gamma     db      '    gamma:           ', 0
+        oxige     db      '    Oxigen:          ', 0
+        co2       db      '    CO2:             ', 0
         multi     db      '    Product:         ', 0
         no_bits   dd      12
 
@@ -16,24 +16,38 @@ section .text
 aoc_day03_2:
         push    ebp
         mov     ebp, esp
-        sub     esp, 212
+        sub     esp, 4096
         
         ;; 4   -> File descriptor
         ;; 132 -> Char buffer begin (len=128)
-        ;; 180 -> Int buffer for comparison
-        ;; 184 -> Pointer to int buffer current position
-        ;; 188 -> Counter of int buffer
-        ;; 192 -> Pointer to char buffer current position
-        ;; 196 -> Epsilon
-        ;; 200 -> Gamma
-        ;; 204 -> Product
+        ;; 136 -> Counter of 0s and 1s (oxygen)
+        ;; 140 -> Bit counter
+        ;; 144 -> Oxygen
+        ;; 148 -> CO2
+        ;; 152 -> Bitmask
+        ;; 156 -> Number
+        ;; 160 -> Counter of 0s and 1s (co2)
+        ;; 164 -> Counter of co2 lines read
+        ;; 168 -> Last co2 line read as integer
+        ;; 172 -> Product
         ;;  +4 -> argument 2
         ;;  +8 -> argument 1
 
-        ;; Print part1
-        mov     dword [esp], part1
+        ;; Print part
+        mov     dword [esp], part
         call    printcstr
         call    printlb
+
+        ;; Set bit counter to number of bits
+        mov     eax, [no_bits]
+        mov     dword [ebp-140], eax
+        ;; Set oxygen & co2 & bitmask to 0
+        mov     dword [ebp-144], 0
+        mov     dword [ebp-148], 0
+        mov     dword [ebp-152], 0
+
+        ;; Read file as many times as necessary
+begin_read_file:
 
         ;; Open file and store file descriptor
         mov     eax, 5
@@ -51,18 +65,15 @@ aoc_day03_2:
         ; call    printd
         ; call    printlb
 
-        ;; Set int buffet to zero
-        lea     eax, [ebp-180]
-        lea     ebx, [ebp-132]
-begin_set_zero:
-        mov     dword [eax], 0
-        add     eax, 4
-        cmp     eax, ebx
-        jge     end_set_zero
-        jmp     begin_set_zero
-end_set_zero:
+        ;; Decrease bit counter
+        sub     dword [ebp-140], 1
 
-begin_get_lines:
+        ;; Set 0s-1s counter to 0 (oxyg and co2)
+        mov     dword [ebp-136], 0
+        mov     dword [ebp-160], 0
+        ;; Counter of co2 lines read
+        mov     dword [ebp-164], 0
+begin_get_line:
         ;; Get line
         mov     eax, [ebp-4]
         mov     [esp], eax
@@ -71,124 +82,128 @@ begin_get_lines:
         call    file_read_line
 
         cmp     eax, 0
-        je      end_get_lines
+        je      end_get_line
 
-        ; ;; Print line
-        ; lea     eax, [ebp-132]
-        ; mov     [esp], eax
-        ; call    printcstr
-
-        ;; Set pointers and counter of int buffer
-        lea     eax, [ebp-180]
-        mov     [ebp-184], eax
-        
+        ;; Convert to number
         lea     eax, [ebp-132]
-        mov     [ebp-192], eax
+        mov     [esp], eax
+        call    cstr_read_bits
+        mov     eax, [esp]
+        mov     [ebp-156], eax
 
-        mov     dword [ebp-188], 0
-begin_append:
-        ;; If '0' sub 1, if '1' add 1, else exit all loops
-        mov     eax, [ebp-192]
-        mov     al, [eax]
-        cmp     al, '0'
-        jne     try_1
-        
-        mov     eax, [ebp-184]
-        mov     ebx, [eax]
-        sub     ebx, 1
-        mov     [eax], ebx
+        ;; Compare with oxygen value
+        mov     eax, [ebp-144] ;; oxygen
+        and     eax, [ebp-152]
+        mov     ebx, [ebp-156] ;; current read val
+        and     ebx, [ebp-152]
+        cmp     eax, ebx
+        jne     dont_compare_oxygen
+        ;; If they're equal, add or sub to 1s-0s counter
+        mov     cl,  [ebp-140] ;; Bit counter
+        mov     ebx, [ebp-136] ;; 1s-0s counter
+        mov     eax, 1
+        shl     eax, cl
+        and     eax, [ebp-156]
+        shr     eax, cl
+        imul    eax, 2
+        sub     eax, 1
+        add     ebx, eax
+        mov     [ebp-136], ebx
+dont_compare_oxygen:
 
-        jmp     exit_try
-try_1:
-        cmp     al, '1'
-        jne     try_none
-        
-        mov     eax, [ebp-184]
-        mov     ebx, [eax]
-        add     ebx, 1
-        mov     [eax], ebx
+        ;; Compare with co2 value
+        mov     eax, [ebp-148] ;; co2
+        and     eax, [ebp-152]
+        mov     ebx, [ebp-156] ;; current read val
+        and     ebx, [ebp-152]
+        cmp     eax, ebx
+        jne     dont_compare_co2
+        ;; If they're equal, add or sub to 1s-0s counter
+        mov     cl,  [ebp-140] ;; Bit counter
+        mov     ebx, [ebp-160] ;; 1s-0s counter
+        mov     eax, 1
+        shl     eax, cl
+        and     eax, [ebp-156]
+        shr     eax, cl
+        imul    eax, 2
+        sub     eax, 1
+        add     ebx, eax
+        mov     [ebp-160], ebx
+        ;; Get the last line read
+        mov     eax, [ebp-156]
+        mov     [ebp-168], eax
 
-        jmp     exit_try
-try_none:
-        jmp     end_get_lines
-exit_try:
+dont_compare_co2:
 
-        ; Add to pointers and 1 to counter
-        mov     eax, [ebp-184]
-        add     eax, 4
-        mov     [ebp-184], eax
+        ;; Read another line
+        jmp     begin_get_line
 
-        mov     eax, [ebp-192]
-        add     eax, 1
-        mov     [ebp-192], eax
+end_get_line:
 
-        mov     eax, [ebp-188]
-        add     eax, 1
-        mov     [ebp-188], eax
+        ;; Get next bit of oxygen value
+        mov     eax, [ebp-144] ;; oxyg
+        mov     ebx, [ebp-136] ;; 0s-1s counter
+        mov     cl,  [ebp-140] ;; bit counter
+        cmp     ebx, 0
+        jl      more_0_ox
+more_1_ox:
+        mov     edx, 1
+        shl     edx, cl
+        or      eax, edx
+        mov     [ebp-144], eax
+more_0_ox:
 
-        ; ;; If counter is equal to number of bits, exit append loop
-        mov     eax, [ebp-188]
-        cmp     eax, [no_bits]
-        jge     end_append
-        
-        ;; Else, go back to begin
-        jmp     begin_append  
-
-end_append:
-
-        jmp     begin_get_lines
-
-end_get_lines:
-        ; call    printlb
-
-        ;; Read int buffer
-        mov     dword [ebp-196], 0 ;; epsilon
-        mov     dword [ebp-200], 0 ;; gamma
-        lea     eax, [ebp-180]
-        mov     cl, [no_bits]
-begin_read_int:
-        sub     cl, 1
-        cmp     dword [eax], 0
-        jl      more_0
-more_1:
-        mov     ebx, 1
-        shl     ebx, cl
-        or      [ebp-196], ebx
-        jmp     end_comp
-more_0:
-        mov     ebx, 1
-        shl     ebx, cl
-        or      [ebp-200], ebx
-        jmp     end_comp
-end_comp:
-        add     eax, 4
-        cmp     cl, 0
-        jle     end_read_int
-        jmp     begin_read_int
-end_read_int:
+        ;; Get next bit of co2 value
+        mov     eax, [ebp-148] ;; co2
+        mov     ebx, [ebp-160] ;; 0s-1s counter
+        mov     cl,  [ebp-140] ;; bit counter
+        cmp     ebx, 0
+        jge     more_1_co2
+more_0_co2:
+        mov     edx, 1
+        shl     edx, cl
+        or      eax, edx
+        mov     [ebp-148], eax
+more_1_co2:
 
         ;; Close file
         mov     eax, 6
         mov     ebx, [ebp-4]
         int     0x80
 
-        ;; Calculate product
-        mov     eax, [ebp-196]
-        imul    eax, [ebp-200]
-        mov     [ebp-204], eax
+        ;; Increase bitmask
+        mov     eax, [ebp-152]
+        mov     cl,  [ebp-140]
+        mov     ebx, 1
+        shl     ebx, cl
+        or      eax, ebx
+        mov     [ebp-152], eax
 
-        ;; Print epsilon
-        mov     dword [esp], epsil
+        cmp     dword [ebp-140], 0
+        jg      begin_read_file
+end_read_file:
+
+        ;; Actual co2 value is the last read value
+        mov     eax, [ebp-168]
+        mov     [ebp-148], eax
+
+        ;; Calculate product
+        mov     eax, [ebp-144]
+        imul    eax, [ebp-148]
+        mov     [ebp-172], eax
+
+        ;; Print oxygen
+        mov     dword [esp], oxige
         call    printcstr
-        mov     eax, [ebp-196]
+        mov     eax, [ebp-144]
         mov     [esp], eax
         call    printd
         call    printlb
 
-        ;; Print gamma
-        mov     dword [esp], gamma
+        ;; Print co2
+        mov     dword [esp], co2
         call    printcstr
-        mov     eax, [ebp-200]
+        mov     eax, [ebp-148]
         mov     [esp], eax
         call    printd
         call    printlb
@@ -196,7 +211,7 @@ end_read_int:
         ;; Print product
         mov     dword [esp], multi
         call    printcstr
-        mov     eax, [ebp-204]
+        mov     eax, [ebp-172]
         mov     [esp], eax
         call    printd
         call    printlb
